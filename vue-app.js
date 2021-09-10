cf769f47b923ff8417e4fe5bbe01ddc0acf546ec
honk <github@honk.digital>
2021-09-10 22:28:32 +0200

    Updated the viewer.

--- vue-app.js
+++ vue-app.js
@@ -2,7 +2,7 @@ const app = new Vue({
     el: '#app',
     data: {
         idclass: 'fonts-lato-300',
-        inputclass: 'input-dot-1',
+        inputclass: 'input-dot-100',
         buttonclass: 'button-round-14',
         message: 'The quick brown fox jumps over the lazy dog'
     }
