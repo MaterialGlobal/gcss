a84960642fb5f8e3c38383503ec237b59695b839
honk <github@honk.digital>
2021-09-10 23:19:47 +0200

    GCSS Viewer v1.0.0 release

--- vue-app.js
+++ vue-app.js
@@ -2,6 +2,7 @@ const app = new Vue({
     el: '#app',
     data: {
         idclass: 'fonts-lato-300',
+        idbuttonclass: 'fonts-lato-400',
         inputclass: 'inputs-dot-100',
         message1: 'The quick brown fox jumps over the lazy dog',
         message2: 'abc'
